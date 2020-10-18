ead1badbe87350191f1f0bbabd5c1e6691fd116f
MironGindis <63605958+MironGindis@users.noreply.github.com>
2020-10-18 16:59:32 +0500

    Add files via upload

---
+++ build/precache-manifest.ce49690dabb315aa34ec699b22c08aaf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46678b0a0177c78c7e8667413d1ce787",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "e2c0dbae95b6a7a26d74",
+    "url": "/site/static/css/main.f6af3650.chunk.css"
+  },
+  {
+    "revision": "8904cab855689bae8f00",
+    "url": "/site/static/js/2.c171652c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/site/static/js/2.c171652c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c0dbae95b6a7a26d74",
+    "url": "/site/static/js/main.308d88cd.chunk.js"
+  },
+  {
+    "revision": "9452bef43a64b3097bf9",
+    "url": "/site/static/js/runtime-main.0e2b5872.js"
+  },
+  {
+    "revision": "2f4fcdbe8a87d8b6dc6d1411b6518f6f",
+    "url": "/site/static/media/DownArrow.2f4fcdbe.svg"
+  },
+  {
+    "revision": "03d1aedc96f0a51535d2407571dd00db",
+    "url": "/site/static/media/Photo.03d1aedc.png"
+  },
+  {
+    "revision": "5a99472081d39591f534d9f5cc28c63d",
+    "url": "/site/static/media/biglogosection.5a994720.svg"
+  },
+  {
+    "revision": "19599e4faff18565b4c2040c30f5f5c5",
+    "url": "/site/static/media/crosses1.19599e4f.svg"
+  },
+  {
+    "revision": "e5cebd0b8bb39d7cd6e13555b98dd14b",
+    "url": "/site/static/media/crosses2.e5cebd0b.svg"
+  },
+  {
+    "revision": "a8efdc70b9bf976702de42ffafca2776",
+    "url": "/site/static/media/crosses3.a8efdc70.svg"
+  },
+  {
+    "revision": "081c64cb5b7a12a2a4cf3c7ae417f02b",
+    "url": "/site/static/media/fingerprint.081c64cb.svg"
+  },
+  {
+    "revision": "e4148c1f25a4b2c63f3518bdd04d3808",
+    "url": "/site/static/media/instlogo.e4148c1f.svg"
+  },
+  {
+    "revision": "fb8377a51265c6a351344199fe1ad987",
+    "url": "/site/static/media/mainlogo.fb8377a5.svg"
+  },
+  {
+    "revision": "3be645a856f90131834311d261a51ab0",
+    "url": "/site/static/media/redlogo.3be645a8.svg"
+  },
+  {
+    "revision": "8e3a71db84eb56f76a11b1890ce3176a",
+    "url": "/site/static/media/secondblockClosses.8e3a71db.svg"
+  },
+  {
+    "revision": "d435a9a3bb2fe5f72a3416ab2bdd53ad",
+    "url": "/site/static/media/secondblockText.d435a9a3.svg"
+  },
+  {
+    "revision": "cc36ec1d7380b53d95fa07eccb09a87a",
+    "url": "/site/static/media/secondblockbg.cc36ec1d.jpg"
+  },
+  {
+    "revision": "a9f8235316d91ce4d9926ffd7d67f885",
+    "url": "/site/static/media/wecan.a9f82353.svg"
+  },
+  {
+    "revision": "a8e84b4585e3d55f03081273407172ee",
+    "url": "/site/static/media/whitelogo.a8e84b45.svg"
+  }
+]);
